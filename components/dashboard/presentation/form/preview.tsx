051439667a6f0b50c77fbb6b6c55894da0a789d0
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-08-12 10:10:50 +0700

    Update preview.tsx

--- components/dashboard/presentation/form/preview.tsx
+++ components/dashboard/presentation/form/preview.tsx
@@ -1,11 +1,19 @@
 import { Card } from "@mantine/core";
 import { useElementSize } from "@mantine/hooks";
+import { Prisma } from "@prisma/client";
 import { useEffect, useState } from "react";
 import { Payload } from "./_payload";
 
 type Props = { slide: Payload["Slide"][number] };
 
 const html = String.raw;
+const escapeHtml = (text?: string) => {
+  if (!text) return;
+  const element = document.createElement("p");
+  const textNode = document.createTextNode(text);
+  element.appendChild(textNode);
+  return element.innerHTML;
+};
 
 export default function Preview(props: Props) {
   const effectDeps = JSON.stringify(props);
@@ -19,10 +27,50 @@ export default function Preview(props: Props) {
       #background {
         display: flex;
         flex-direction: column;
+        justify-content: center;
+        align-items: center;
         background-color: white;
+        height: 100%;
         color: black;
         #title {
-          font-size: 24px;
+          font-size: 32px;
+          text-align: center;
+        }
+        #subtitle {
+          font-size: 16px;
+          text-align: center;
+        }
+      }
+    </style>
+    <foreignObject
+      width="${width}"
+      height="${height}"
+    >
+      <div
+        xmlns="http://www.w3.org/1999/xhtml"
+        id="background"
+      >
+        <div id="title">${escapeHtml(props.slide.TitleSlide?.title)}</div>
+        <div id="subtitle">${props.slide.TitleSlide?.subtitle}</div>
+      </div>
+    </foreignObject>
+  </svg>`;
+  const titleAndContent = html`<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="${width}"
+    height="${height}"
+  >
+    <style>
+      #background {
+        display: flex;
+        flex-direction: column;
+        background-color: white;
+        padding: 12px;
+        height: 100%;
+        color: black;
+        #title {
+          font-size: 32px;
+          text-align: center;
         }
         #subtitle {
           font-size: 16px;
@@ -37,16 +85,27 @@ export default function Preview(props: Props) {
         xmlns="http://www.w3.org/1999/xhtml"
         id="background"
       >
-        <div id="title">${props.slide.TitleSlide?.title}</div>
-        <div id="subtitle">${props.slide.TitleSlide?.subtitle}</div>
+        <div id="title">${escapeHtml(props.slide.TitleAndContent?.title)}</div>
+        <div id="subtitle">${props.slide.TitleAndContent?.content}</div>
       </div>
     </foreignObject>
   </svg>`;
   const src = useState("");
   useEffect(() => {
-    const url = URL.createObjectURL(
-      new Blob([titleSlide], { type: "image/svg+xml" }),
-    );
+    let blob;
+    switch (props.slide.type) {
+      case Prisma.ModelName.TitleSlide:
+        blob = new Blob([titleSlide], { type: "image/svg+xml" });
+        break;
+      case Prisma.ModelName.TitleAndContent:
+        blob = new Blob([titleAndContent], { type: "image/svg+xml" });
+        break;
+      case Prisma.ModelName.SectionHeader:
+        break;
+    }
+    if (!blob) return;
+    const url = URL.createObjectURL(blob);
+    console.log({ url });
     src[1](url);
     return () => {
       URL.revokeObjectURL(url);
